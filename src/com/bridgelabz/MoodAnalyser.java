63b03986d1e578b23206dd84cfa81194cdba73ea
Shahid-sk <mail2sk.shahid@gmail.com>
2021-09-18 19:02:25 +0530

    added: created a class to analyse and respond mood

---
+++ src/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+public class MoodAnalyser {
+
+	/**
+	 * Purpose : Given a message, ability to analyse and respond HAPPY or SAD mood
+	 *
+	 * @parameter : message
+	 * @return : HAPPY or SAD
+	 */
+	public String analyseMood(String message) {
+		if (message.contains("sad"))
+			return "SAD";
+		else
+			return "HAPPY";
+	}
+
+}
